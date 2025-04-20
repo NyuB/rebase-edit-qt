890d8b7ba6d2d40d378bf2469113b0cc20926fca
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-04-20 09:49:33 +0200

    Use testing namespace

--- test.cpp
+++ test.cpp
@@ -3,9 +3,12 @@
 #include <gtest/gtest.h>
 #include <limits>
 
+using testing::Eq;
+using testing::Values;
+
 TEST(HelloTest, SimpleAssert) { ASSERT_EQ(7 * 7, 49); }
 
-TEST(HelloTest, AssertThat) { ASSERT_THAT(7 * 6, ::testing::Eq(42)); }
+TEST(HelloTest, AssertThat) { ASSERT_THAT(7 * 6, Eq(42)); }
 
 struct AddTestParam {
   int left;
@@ -20,8 +23,7 @@ TEST_P(AddTest, Add) {
 }
 
 INSTANTIATE_TEST_SUITE_P(HappyPaths, AddTest,
-                         ::testing::Values(AddTestParam{1, 2, 3},
-                                           AddTestParam{2, 1, 3}));
+                         Values(AddTestParam{1, 2, 3}, AddTestParam{2, 1, 3}));
 INSTANTIATE_TEST_SUITE_P(ExtremeValues, AddTest,
-                         ::testing::Values(AddTestParam{1, -1, 0},
-                                           AddTestParam{INT_MAX, 1, INT_MIN}));
+                         Values(AddTestParam{1, -1, 0},
+                                AddTestParam{INT_MAX, 1, INT_MIN}));
